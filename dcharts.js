697251ddaf8fd0a81de24b76daff96ffccd6f08f
handengwei <sword_13@163.com>
2017-03-03 15:35:04 +0800

    optimize tooltip position

--- dcharts.js
+++ dcharts.js
@@ -197,9 +197,6 @@
                 var y = d3.event.pageY;
                 _this._moveTooltip(_selector, x, y);
               });
-              // .on('mouseleave', function(d) {
-              //   _this._hideTooltip(d, _selector);
-              // });
 
               _bodyG.selectAll("text.text")
                       .data(_data)
@@ -297,15 +294,22 @@
                         .html(d.y);
     },
     _moveTooltip: function(_selector, x, y) {
-      var _tooltip = _selector.select('div.tooltip')
-                        .transition()
-                        .style('left', x + 'px')
-                        .style('top', y + 20 + 'px')
+      var
+      _tooltip = _selector.select('div.tooltip'),
+      main_width = parseFloat(_selector.style('width')),
+      main_height = parseFloat(_selector.style('height')),
+      self_width = parseFloat(_tooltip.style('width')) + 30,
+      self_height = parseFloat(_tooltip.style('height')) + 30,
+      x = (x >= (main_width/2)) ? x - self_width : x,
+      y = (y >= (main_height/2)) ? y - self_height : y;
+
+      _tooltip.style('left', x + 'px')
+              .style('top', y + 10 + 'px');
     },
     _hideTooltip: function(d, _selector) {
       var _tooltip = _selector.select('div.tooltip')
                         .transition()
-                        .style('opacity', 0)
+                        .style('opacity', 0);
     },
     _initTooltip: function(_selector) {
       var _tooltip;
